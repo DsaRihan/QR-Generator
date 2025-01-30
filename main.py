3c4dfb6eeb1736341867740315b06c49b49a92e4
Rihan <100439488+DsaRihan@users.noreply.github.com>
2025-01-30 12:26:42 +0530

    modified

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, render_template, send_file
-import qrcode 
+import qrcode as qr
 import os
-from PIL import Image
 
 app = Flask(__name__)
 
@@ -12,16 +11,13 @@ def index():
 @app.route('/generate', methods=['POST'])
 def generate():
     url = request.form['url']
-    qrc = qrcode.QRCode(version=1, error_correction=qrcode.ERROR_CORRECT_H, box_size=10, border=4)
+    qrc = qr.QRCode(version=1, error_correction=qr.ERROR_CORRECT_H, box_size=10, border=4)
     qrc.add_data(url)
     qrc.make(fit=True)
     img = qrc.make_image(fill_color="red", back_color="black")
-    img_path = "qr.png"
+    img_path = os.path.join('qr_images', 'qr.png')  # Updated path to save in qr_images directory
     img.save(img_path)
     return send_file(img_path, mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
